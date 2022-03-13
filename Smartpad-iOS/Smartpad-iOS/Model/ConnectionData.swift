38839ea41a79be6f212bb838536b2d9660562267
Alireza Azimi <46511311+DJMaest@users.noreply.github.com>
2022-03-13 11:02:58 -0600

    add model and unit tests (#5)
    Co-authored-by: Alireza Azimi <alirezaazimi@Alirezas-MacBook-Pro.local>

---
+++ Smartpad-iOS/Smartpad-iOS/Model/ConnectionData.swift
@@ -0,0 +1,31 @@
+//
+//  ConnectionData.swift
+//  Smartpad-iOS
+//
+//  Created by Alireza Azimi on 2022-03-13.
+//
+
+import Foundation
+enum StorageError: Error {
+    case ValueNotFoundError
+}
+struct ConnectionKeys {
+    static let deviceUUID = ""
+}
+
+class ConnectionData {
+    let defaults = UserDefaults.standard
+    
+    func setSelectedDeviceUUID(uuid: String){
+        defaults.set(uuid, forKey: ConnectionKeys.deviceUUID)
+    }
+    
+    func getSelectedDeviceUUID() throws -> String {
+        if let deviceUUID = defaults.string(forKey: ConnectionKeys.deviceUUID) {
+           return deviceUUID
+        } else {
+            throw StorageError.ValueNotFoundError
+        }
+        
+    }
+}
