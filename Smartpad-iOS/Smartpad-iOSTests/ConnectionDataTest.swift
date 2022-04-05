f2a044b251d663ed75e05f377de8a630d14885b8
Alireza Azimi <46511311+DJMaest@users.noreply.github.com>
2022-04-05 12:44:35 -0600

    add tests (#38)
    Co-authored-by: Alireza Azimi <alirezaazimi@Alirezas-MacBook-Pro.local>

--- Smartpad-iOS/Smartpad-iOSTests/ConnectionDataTest.swift
+++ Smartpad-iOS/Smartpad-iOSTests/ConnectionDataTest.swift
@@ -30,6 +30,74 @@ class ConnectionDataTest: XCTestCase {
             defaults.removeObject(forKey: key)
         }
     }
-
-
+    
+    func testSetCurrentDeviceName(){
+        // Arrange
+        let data = ConnectionData()
+        // Act
+        let name = "my iPhone"
+        data.setDeviceName(name: name)
+        //Assert
+        XCTAssertEqual(UserDefaults.standard.string(forKey: ConnectionKeys.currDeviceName), "my iPhone")
+    }
+    
+    func testGetCurrentDeviceName(){
+        //Arrange
+        resetDefaults()
+        let data = ConnectionData()
+        // Act
+        let name = "my iPhone"
+        data.setDeviceName(name: name)
+        //Assert
+        XCTAssertEqual(data.getDeviceName(), "my iPhone")
+        
+    }
+    
+    func testSetCurrentDeviceUUID(){
+        // Arrange
+        resetDefaults()
+        let uuidString = UUID().uuidString
+        let data = ConnectionData()
+        // Act
+        data.setCurrentDeviceUUID(uuid: uuidString)
+        // Assert
+        XCTAssertEqual(UserDefaults.standard.string(forKey: ConnectionKeys.currDeviceUUID), uuidString)
+        
+        
+    }
+    
+    func testGetCurrentDeviceUUID(){
+        // Arrange
+        resetDefaults()
+        let uuidString = UUID().uuidString
+        let data = ConnectionData()
+        // Act
+        data.setCurrentDeviceUUID(uuid: uuidString)
+        // Assert
+        XCTAssertEqual(data.getCurrentDeviceUUID(), uuidString)
+        
+    }
+    
+    func testSetPeerName(){
+        // Arrange
+        resetDefaults()
+        let data = ConnectionData()
+        // Act
+        data.setSelectedPeer(name: "Ali's macbook")
+        // Assert
+        XCTAssertEqual(UserDefaults.standard.string(forKey: ConnectionKeys.selectedPeerName), "Ali's macbook")
+        
+    }
+    
+    func testGetPeerName(){
+        // Arrange
+        resetDefaults()
+        let data = ConnectionData()
+        // Act
+        data.setSelectedPeer(name: "Ali's macbook")
+        // Assert
+        XCTAssertEqual(data.getSelectedPeer(), "Ali's macbook")
+        
+    }
+    
 }
